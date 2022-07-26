1ac81aa0c32b85b0d71aa433d0e76ac6ad81cbf6
Dream Garden (rey) <93763859+HELZRIP@users.noreply.github.com>
2022-07-26 10:50:21 +0700

    Create apakah.py

---
+++ AsukaRobot/modules/apakah.py
@@ -0,0 +1,26 @@
+import random
+from AsukaRobot.events import register
+from AsukaRobot import telethn
+
+APAKAH_STRING = [
+    "Iya",
+    "Tidak",
+    "Mungkin",
+    "Mungkin Tidak",
+    "Bisa jadi",
+    "Mungkin Tidak",
+    "Tidak Mungkin",
+    "YNTKTS",
+    "Ya gk Tau Kok Tanya Saya",
+    "Apa iya?",
+    "Tanya aja sama mamak kau tu pler",
+]
+
+
+@register(pattern="^/apakah ?(.*)")
+async def apakah(event):
+    quew = event.pattern_match.group(1)
+    if not quew:
+        await event.reply("Berikan saya pertanyaan paham 😑😑")
+        return
+    await event.reply(random.choice(APAKAH_STRING))
