605eef1c1e45fd064d8f9d8b373f3762461d6363
Dream Garden (rey) <93763859+HELZRIP@users.noreply.github.com>
2022-08-01 23:43:38 +0700

    Update __main__.py

--- AsukaRobot/__main__.py
+++ AsukaRobot/__main__.py
@@ -155,7 +155,7 @@ Asuka_N_IMG = (
 
 Asuka_PIC = "https://telegra.ph/file/4de2305a9213f9db86125.jpg"
 
-Asuka_VID = "https://telegra.ph/file/4de2305a9213f9db86125.jpg"
+Asuka_VID = "https://telegra.ph/file/03a861352384cee8adc68.mp4"
 
 PM_PHOTO = "https://te.legra.ph/file/5e16d1ce34d58bca8bdde.jpg"
 
@@ -316,7 +316,7 @@ def start(update: Update, context: CallbackContext):
         first = update.effective_user.full_name
         chat = update.effective_chat.title
         update.effective_message.reply_video(
-                video="https://telegra.ph/file/4de2305a9213f9db86125.jpg",
+                video="https://telegra.ph/file/03a861352384cee8adc68.mp4",
                 caption="Yes Darling, I Am Alive And Working Fine. \nCheck Out The Buttons Mentioned Below.",
             parse_mode=ParseMode.HTML,
             reply_markup=InlineKeyboardMarkup(
