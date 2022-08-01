dc955d577b1c07dd5afa35e2eedb257060047239
Dream Garden (rey) <93763859+HELZRIP@users.noreply.github.com>
2022-08-01 23:45:46 +0700

    Update afk.py

--- AsukaRobot/modules/afk.py
+++ AsukaRobot/modules/afk.py
@@ -20,7 +20,7 @@ from AsukaRobot.modules.helper_funcs.readable_time import get_readable_time
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
 
-AFKVID = "https://telegra.ph/file/20fbebe521160c5464be6.jpg"
+AFKVID = "https://telegra.ph/file/03a861352384cee8adc68.mp4"
 
 
 def afk(update, context):
