fbf00bd69817b75c668c4799ca789b7533c1a32d
Dream Garden (rey) <93763859+HELZRIP@users.noreply.github.com>
2022-08-02 10:17:25 +0700

    Update hentai_strings.py

--- AsukaRobot/modules/hentai_strings.py
+++ AsukaRobot/modules/hentai_strings.py
@@ -1,30 +1,12 @@
+#These strings are specifically for Hentai Module.
+
+
 HENTAIPIC_STRINGS = (
 	"ara ara yameteh /hentaipic klik again",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-        "",
+	"coming soon will update",
 )
 
 HENTAIVID_STRINGS = (
 	"ara ara yameteh /hentaivid klik again",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
+	"coming soon will update",
 )
